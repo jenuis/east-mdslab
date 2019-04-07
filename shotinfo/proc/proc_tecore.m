49a67fad4f65351094c0c1a9d524aeaf0a51866a
Xiang Liu <jent.le@hotmail.com>
2019-04-07 16:40:17 +0800

    fix shotinfo/proc/proc_tecore.m

--- shotinfo/proc/proc_tecore.m
+++ shotinfo/proc/proc_tecore.m
@@ -1,6 +1,12 @@
-function te = proc_tecore(shotno, time_range)
+function te = proc_tecore(shotno, time_range, ece_type)
 if nargin == 1
     time_range = [];
+    ece_type = 'hrs';
+elseif nargin == 2
+    ece_type = 'hrs';
 end
-mi = profile(shotno, 'mi');
-mi.loadbycal(time_range)
+if ~haselement({'hrs','mi'},ece_type)
+    error('ECE Type wrong, should be "hrs" or "mi"!')
+end
+signal_name = ['te0_' ece_type];
+te = signal_proc(shotno, 'Analysis', signal_name, time_range);
