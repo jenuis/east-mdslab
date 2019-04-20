e853db2aaf79fefea0514d619082621d07cdbac7
Xiang Liu <jent.le@hotmail.com>
2019-04-20 11:41:22 +0800

    prettify flattop and add comments

--- common/analy/flattop.m
+++ common/analy/flattop.m
@@ -1,28 +1,47 @@
-function iRange = flattop(dbData,dbStartPercent,dbIncreasment,dbDecreasePercent)
 %% Find steady range of input data
 % Author: Xiang Liu@ASIPP
 % Last Modified: 2014-08-07
 % E-mail: jent.le@hotmail.com
 % Version : 1.0
+function flat_ind_rng = flattop(signal_data, thres_init, thres_increas_step, stop_criteria)
+%% By populating the number of data that larger than a certain threshold to 
+% extract information of the flat-top region of the input signal.
+% 
+% output:
+%     flat_ind_rng: the indices of the start and end of the flat-top
+%         region.
+% input:
+%     singal_data: signal data, for instance, the plasma current.
+%     thres_init: the ratio between initial value to the peak value.
+%         Default: 0.85.
+%     thres_increas_step: the increasing step of the threshold. Default:
+%         0.01.
+%     stop_criteria: the stop criteria, where the ratio between the next- 
+%         step population and the current-step popluation is smaller than
+%         this value. Default: 0.9.
+%% check arguments
 if nargin == 1
-    dbStartPercent = 0.85;
-    dbIncreasment = 0.01;
-    dbDecreasePercent = 0.9;
-else
-    dbDecreasePercent = 1 - dbDecreasePercent;
+    thres_init = 0.85;
+    thres_increas_step = 0.01;
+    stop_criteria = 0.9;
+elseif nargin == 2
+    thres_increas_step = 0.01;
+    stop_criteria = 0.9;
+elseif nargin == 3
+    stop_criteria = 0.9;
 end
-dbMax = max(dbData);
-iIndex = find(dbData >= dbMax*dbStartPercent);
-iLen = length(iIndex);
-for dbThres = dbStartPercent+dbIncreasment:dbIncreasment:1
-    TempIndex = find(dbData >= dbMax*dbThres);
-    iTempLen = length(TempIndex);
-    if iTempLen > iLen*dbDecreasePercent
-        iIndex = TempIndex;
-        iLen = iTempLen;
+%% main
+data_max = max(signal_data); % max value of input signal data
+inds = find(signal_data >= data_max*thres_init); % indices that larger than the initial value
+population = length(inds); % the total number of values that larger than the initial value
+for thres = thres_init+thres_increas_step:thres_increas_step:1 % increase threshold
+    inds_next = find(signal_data >= data_max*thres); % next step indices
+    popluation_next = length(inds_next); % population of the next step indices
+    if popluation_next > population*stop_criteria % check stop criteria
+        inds = inds_next;
+        population = popluation_next;
     else
-        break
+        break % stop criteria satisfied
     end
 end
-iRange = [min(iIndex) max(iIndex)];
-end
+flat_ind_rng = [min(inds) max(inds)]; % assign output
