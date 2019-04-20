5fd458e2c1730c3019aeecb45863f06424dd29fc
Xiang Liu <jent.le@hotmail.com>
2019-04-20 18:30:37 +0800

    add missing file, adata_info.m

---
+++ divlp/adata/adata_info.m
@@ -0,0 +1,44 @@
+global DataType
+if ~isempty(DataType)
+    return
+end
+DataType = {
+    '\lambda_{js}'...             %1
+    '\lambda std'...              %2
+    'I_{p}'...                    %3
+    'B_{p}'...                    %4
+    'B_{t}'...                    %5
+    'n_{e}'...                    %6
+    'P_{input}'...                %7
+    '\kappa'...                   %8
+    '\delta_{top}'...             %9
+    'q_{95}'...                   %10
+    'L_{leg,in}'...               %11
+    'L_{leg,out}'...              %12
+    'shotno'...                   %13
+    'S_{js}'...                   %14
+    'S std'...                    %15
+    'Powbit'...                   %16
+    'j_{s,peak}'...               %17
+    '\lambda_{int,Eich}'...       %18
+    'W_{MHD}'...                  %19
+    'D_{\alpha}'...               %20
+    'R^2'...                      %21
+    't'...                        %22
+    'P_{ECRH}'...                 %23
+    'P_{ICRF}'...                 %24
+    'P_{LHW}'...                  %25
+    'P_{NBI}'...                  %26
+    'T_{e,tar}'...                %27
+    'r_0'...                      %28
+    'R'...                        %29
+    'a'...                        %30
+    'V_{loop}'...                 %31
+    'n_{e,edge}'...               %32
+    'P_{rad}'...                  %33
+    'T_{e,core}'...               %34
+    '\beta_p'...                  %35
+    '\beta_t'...                  %36
+    '\lambda_{int}'...            %37   
+    '\delta_{bottom}'...          %38
+    };
