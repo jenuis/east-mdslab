6f9524d64a5efac1f1435f7e92425e846f6c7d5d
Xiang Liu <jent.le@hotmail.com>
2019-07-18 13:20:19 +0800

    add foldershotlist.m

---
+++ common/fileio/foldershotlist.m
@@ -0,0 +1,47 @@
+function [shotlist, filelist] = foldershotlist(Dirs, FilterStr, shotno_ind)
+if nargin == 2
+    shotno_ind = 5:9;
+end
+if ischar(Dirs)
+    [shotlist, filelist] = listone(Dirs, FilterStr, shotno_ind);
+elseif iscellstr(Dirs)
+    [shotlist, filelist] = listmany(Dirs, FilterStr, shotno_ind);
+else
+    error('Bad input type!')
+end
+if iscell(shotlist) && length(shotlist) == 1
+    shotlist = shotlist{1};
+    filelist = filelist{1};
+end
+
+
+% if ~no_sort
+%     shotlist = sort(shotlist);
+% end
+
+
+function [shotlist, filelist] = listmany(Dirs, FilterStr, shotno_ind)
+if ~iscellstr(Dirs)
+    error('Only path accepted!')
+end
+shotlist = {};
+filelist = {};
+for i=1:length(Dirs)
+    [shotlist{i}, filelist{i}] = listone(Dirs{i}, FilterStr, shotno_ind);
+end
+
+function [shotlist, filelist] = listone(Dirs, FilterStr, shotno_ind)
+if ~ischar(Dirs)
+    error('Only path accepted!')
+end
+dir_list = rdir(fullfile(Dirs), FilterStr);
+shotlist = [];
+filelist = {};
+for i=1:length(dir_list)
+    [~, file_name] = fileparts(dir_list(i).name);
+    filelist{end+1} = file_name;
+    shotlist(i) = str2double(file_name(shotno_ind));
+end
+bad_ind = isnan(shotlist);
+shotlist(bad_ind) = [];
+filelist(bad_ind) = [];
