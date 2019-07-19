dca7877234c3c8869f8323c65f2413020ce2618e
Xiang Liu <jent.le@hotmail.com>
2019-07-19 14:38:01 +0800

    check arguments in for argstr_extract.m

--- common/cell/argstr_extract.m
+++ common/cell/argstr_extract.m
@@ -1,4 +1,12 @@
 function str = argstr_extract(cell_str, str)
+%% check arguments
+if ~ischar(str)
+    error('argument str is not a string!')
+end
+if ~iscellstr(cell_str)
+    error('argument cell_str is not a cellstr')
+end
+%% main
 str = lower(strrmsymbol(str));
 for i=1:length(cell_str)
     cell_str{i} = lower(strrmsymbol(cell_str{i}));
