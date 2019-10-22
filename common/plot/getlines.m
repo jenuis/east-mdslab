17123d55c952bb30f90fe49f7dd902cc1ba8465e
Xiang Liu <jent.le@hotmail.com>
2019-10-22 15:51:00 +0800

    add getlines.m

---
+++ common/plot/getlines.m
@@ -0,0 +1,23 @@
+function line_objs = getlines(axe_obj, line_no, line_type)
+if nargin == 0
+    axe_obj = gca;
+end
+if nargin < 3
+    line_type = 'Line';
+end
+if ischar(line_type)
+    line_type = {line_type};
+end
+line_type = lower(line_type);
+line_objs = findobj(axe_obj);
+line_objs = flip(line_objs);
+bad_ind = [];
+for i=1:length(line_objs)
+    if ~haselement(line_type, line_objs(i).Type)
+        bad_ind(end+1) = i;
+    end
+end
+line_objs(bad_ind) = [];
+if nargin > 1 && ~isempty(line_no)
+    line_objs = line_objs(line_no);
+end
