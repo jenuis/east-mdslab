f3379df33de680de1912614b3b880228d15831ca
Xiang Liu <jent.le@hotmail.com>
2019-10-30 13:24:46 +0800

    revise genpowbit

--- shotinfo/heating/genpowbit.m
+++ shotinfo/heating/genpowbit.m
@@ -1,21 +1,41 @@
 function pow_bit = genpowbit(pow_info, order)
-if nargin == 1
+%% check arguments
+if nargin < 2
     order = {'ecrh', 'icrf', 'lhw', 'nbi'};
-    order_alt = {'ech', 'icrf', 'lhw', 'nbi'};
+end
+if fieldexist(pow_info, 'ech') && ~fieldexist(pow_info, 'ecrh')
+    pow_info.ecrh = pow_info.ech;
+end
+for i=1:length(order)
+    if fieldexist(pow_info, order{i})
+        len = length(pow_info.(order{i}));
+        break
+    end
+end
+for i=1:length(order)
+    name = order{i};
+    if ~fieldexist(pow_info, name)
+        error(['Can not find pow_info.' name]);
+    end
+    if length(pow_info.(name)) ~= len
+        error('different length for data in pow_info!')
+    end
 end
 pow_bit = [];
-for i=1:length(order)
-    try
-        pow = pow_info.(order{i});
-    catch
-        pow = pow_info.(order_alt{i});
+%% recursive call
+if length(pow_info.ecrh) > 1
+    tmp = struct();
+    for i=1:length(pow_info.ecrh)
+        for j=1:length(order)
+            name = order{j};
+            tmp.(name) = pow_info.(name)(i); 
+        end
+        pow_bit(i) = genpowbit(tmp);
     end
-    if pow
-        curr_bit = 1;
-    else
-        curr_bit = 0;
-    end
-    pow_bit(i) = curr_bit;
+    return
 end
-
-pow_bit = bit2dec(pow_bit);
+%% cal pow_bit
+for i=1:length(order)
+    pow_bit(i) =  pow_info.(order{i});
+end
+pow_bit = bit2dec(logical(pow_bit));
