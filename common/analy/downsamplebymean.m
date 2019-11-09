0e9ef07db23518f0bde6288595d4fe65d6111b1a
Xiang Liu <jent.le@hotmail.com>
2019-11-09 10:35:05 +0800

    add averaging method for downsamplebymean

--- common/analy/downsamplebymean.m
+++ common/analy/downsamplebymean.m
@@ -1,10 +1,22 @@
-function array_out = downsamplebymean(array_in, avg_len)
+function array_out = downsamplebymean(array_in, avg_len, avg_method)
 %% average data along row by step of avg_len
 % Xiang Liu@ ASIPP
 % Modified@2017-3-16
 if avg_len == 1
     array_out = array_in;
     return
+end
+if nargin < 3
+    avg_method = 'mean';
+end
+[res, ind] = haselement({'mean', 'median'}, avg_method);
+if ~res
+    error('Not recognized avg method!')
+end
+if ind == 1
+    avg_method = @mean;
+else
+    avg_method = @median;
 end
 shape = size(array_in);
 if shape(2) < shape(1)
@@ -14,6 +26,6 @@ end
 new_len = floor(shape(2)/avg_len);
 array_out = zeros(shape(1),new_len);
 for i=1:shape(1)
-    array_out(i,:) = mean(reshape(array_in(i,1:new_len*avg_len),avg_len,new_len));
+    array_out(i,:) = avg_method(reshape(array_in(i,1:new_len*avg_len),avg_len,new_len));
 end
 end
