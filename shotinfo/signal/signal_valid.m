fba3a34b05bd9e2d8cdd6226adcabad07ea76463
Xiang Liu <jent.le@hotmail.com>
2024-06-03 13:49:33 +0800

    rename ambiguous shotinfo.signal.has_pulse to shotinfo.signal.signal_valid

--- shotinfo/signal/signal_valid.m
+++ shotinfo/signal/signal_valid.m
@@ -1,4 +1,4 @@
-function [status, pulse_time] = has_pulse(sig, varargin)
+function [status, pulse_time] = signal_valid(signal, varargin)
 %% check arguments
 Args.BndRatio = 0.02;
 Args.BgRatio = 0.08;
@@ -20,30 +20,30 @@ Args = parseArgs(varargin, Args, {'Plot'});
 status = 0;
 pulse_time = [];
 %% downsample signal
-sig.time = downsamplebymean(sig.time, Args.DownSamplePoint, Args.DownSampleMethod);
-sig.data = downsamplebymean(sig.data, Args.DownSamplePoint, Args.DownSampleMethod);
+signal.time = downsamplebymean(signal.time, Args.DownSamplePoint, Args.DownSampleMethod);
+signal.data = downsamplebymean(signal.data, Args.DownSamplePoint, Args.DownSampleMethod);
 %% slice signal and get background
-bg = sig.data(1:floor(length(sig.data)*Args.BgRatio));
-inds = sig.time <= Args.BgTimeMax;
+bg = signal.data(1:floor(length(signal.data)*Args.BgRatio));
+inds = signal.time <= Args.BgTimeMax;
 if ~isempty(inds) && sum(inds) > length(bg)
-    bg = sig.data(inds);
+    bg = signal.data(inds);
 end
 if isempty(Args.Ip)
-    inds = sig.time > 0;
-    sig.time = sig.time(inds);
-    sig.data = sig.data(inds);
+    inds = signal.time > 0;
+    signal.time = signal.time(inds);
+    signal.data = signal.data(inds);
 else
-    sig = signal_slice(sig, Args.Ip.flat_time);
+    signal = signal_slice(signal, Args.Ip.flat_time);
 end
 if Args.Plot
     figure(gcf)
-    plot(sig.time, sig.data, 'k-');
+    plot(signal.time, signal.data, 'k-');
     hold on
-    plot(sig.time(1:length(bg)), bg, 'r-')
+    plot(signal.time(1:length(bg)), bg, 'r-')
     xlabel('Time [s]')
 end
 %% check pulse apmlitute
-sig_pp = diff(rangebystat(sig.data, Args.BndRatio));
+sig_pp = diff(rangebystat(signal.data, Args.BndRatio));
 bg_pp = diff(rangebystat(bg, Args.BndRatio));
 if Args.Plot
     h = hline([0 1]*sig_pp+mean(bg)-bg_pp/2); set(h, 'linewidth', 1.5, 'color', 'k');
@@ -57,15 +57,15 @@ if sig_pp < Args.PulseAmpMin ...
     return
 end
 %% check pulse length
-dt = mean(diff(sig.time));
+dt = mean(diff(signal.time));
 
 bg_mean = mean(bg);
-sig_mean = mean(sig.data( sig.data<=bg_mean+bg_pp/2 & sig.data>=bg_mean-bg_pp/2 ));
-inds = sig.data > sig_mean + bg_pp;
+sig_mean = mean(signal.data( signal.data<=bg_mean+bg_pp/2 & signal.data>=bg_mean-bg_pp/2 ));
+inds = signal.data > sig_mean + bg_pp;
 
 inds_sum = sum(inds);
-status = inds_sum/length(sig.time) >= Args.PulseLenMinRatio;
-status = status && inds_sum/length(sig.time) <= Args.PulseLenMaxRatio;
+status = inds_sum/length(signal.time) >= Args.PulseLenMinRatio;
+status = status && inds_sum/length(signal.time) <= Args.PulseLenMaxRatio;
 
 plulse_len = inds_sum*dt;
 status = status && plulse_len >= Args.PulseLenMin;
@@ -73,7 +73,7 @@ status = status && plulse_len <= Args.PulseLenMax;
 
 if Args.Plot
     textbp({num2str(plulse_len, 'pulse len: %.2fs'),...
-        num2str(sig.time(end)-sig.time(1), 'total len: %.2fs'),...
+        num2str(signal.time(end)-signal.time(1), 'total len: %.2fs'),...
         num2str(status, 'status:%i')},...
         'fontsize', 20,...
         'color','b');
@@ -82,7 +82,7 @@ if ~status
     return
 end
 
-pulse_time =sig.time(inds);
+pulse_time =signal.time(inds);
 time_range = rangebystat(pulse_time, 0.1);
 pulse_time = pulse_time(pulse_time >= min(time_range) & pulse_time <= max(time_range));
 
